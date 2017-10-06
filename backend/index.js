5c5fd9aae136ae721d8085fdb7bfd5cb8ada482a
dansmog <juwonanthony@gmail.com>
2017-10-06 01:18:36 +0100

    Basic routes handling

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 //setup express app
 const app = express();
 
+app.get('/api/v1', (req, res) => {
+    console.log('Get request');
+    res.send({ name: "Daniel Anthony Juwon " });
+})
+
 //listen for requests
 app.listen(process.env.port || 4000, () => {
     console.log("now listening for requests");
