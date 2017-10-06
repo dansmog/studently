51a7c69a5cbbe415c8002ee23575fd7412e984b0
dansmog <juwonanthony@gmail.com>
2017-10-06 02:24:16 +0100

    Add student schema and model

---
+++ backend/models/student.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//create student schema and model
+const StudentSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    sex: {
+        type: String,
+        required: [true, 'Sex field is required']
+    },
+    location: {
+        type: String,
+        required: [true, 'Location field is required']
+    },
+    dob: {
+        type: Date,
+        required: [true, 'Date of Birth field is required']
+    },
+    hobby: {
+        type: String,
+    },
+    about: {
+        type: String,
+        required: [true, 'About field is required']
+    },
+    phone: {
+        type: Number,
+        required: [true, 'Phone field is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email field is requied'];
+    },
+    education_qualification: {
+        type: String,
+        required: [true, "Education qualification is required"]
+    },
+    job: {
+        type: String,
+        required: [true, 'Job field is required']
+    }
+
+});
+
+const Student = mongoose.model('student', StudentSchema);
+module.exports = Student;
