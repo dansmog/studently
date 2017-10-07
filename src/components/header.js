c593e79e1be43c0c20be7ba235ecfe11c5589a54
dansmog <juwonanthony@gmail.com>
2017-10-07 10:17:02 +0100

    basic styling for header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,17 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>header</h1>
+        <div className="container">
+            <nav>
+                <ul class="menu d-flex twitter" role="navigation">
+                    <li><a href=""><i className="icons icon-chart"></i>&nbsp;Dashboard</a></li>
+                    <li><a href=""><i className="icons icon-people"></i>Students</a></li>
+                    <li className="menu__bird"><a href=""><i className="fa fa-twitter"></i>studently</a></li>
+                    <li className="btn__add"><a href=""><i className="icons icon-plus"></i>new Student</a></li>
+                    <li><a href=""><i className="icons icon-logout"></i>&nbsp;Log out</a></li>
+                </ul>
+            </nav>
+        </div>
       </header>
     );
   }
